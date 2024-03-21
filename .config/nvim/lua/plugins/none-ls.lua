ca8a0b0775cd1153153a70614af218f9883cf1a8
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-03-21 20:06:56 +0100

    nvim

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,8 +6,6 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua, -- Lua
 				null_ls.builtins.formatting.clang_format, -- C/C++
-				null_ls.builtins.diagnostics.ruff,
-				null_ls.builtins.diagnostics.mypy,
 				null_ls.builtins.formatting.black, -- Python
 				null_ls.builtins.formatting.isort,
 			},
