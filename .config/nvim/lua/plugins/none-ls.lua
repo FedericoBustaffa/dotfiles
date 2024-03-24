eb3d678e51f023215e86a4a9be75b24194a69952
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-03-24 17:47:58 +0100

    vim

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,20 +1,41 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua, -- Lua
-				null_ls.builtins.formatting.clang_format, -- C/C++
-				null_ls.builtins.diagnostics.cppcheck,
-				null_ls.builtins.formatting.black, -- Python
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.diagnostics.pylint.with({
-					only_local = ".env/bin/",
-				}),
-			},
-		})
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					"stylua",
+					"clang_format",
+					"black",
+					"isort",
+					"pylint",
+				},
+			})
+		end,
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua, -- Lua
+					null_ls.builtins.formatting.clang_format, -- C/C++
+					null_ls.builtins.diagnostics.cppcheck,
+					null_ls.builtins.formatting.black, -- Python
+					null_ls.builtins.formatting.isort,
+					null_ls.builtins.diagnostics.pylint.with({
+						only_local = ".env/bin/",
+					}),
+				},
+			})
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
 }
