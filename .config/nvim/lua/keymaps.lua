a663bce1cd05f88930b84a5782d073f3df6b20c2
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-03-25 02:28:01 +0100

    dots

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,4 +1,3 @@
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
@@ -10,21 +9,39 @@ vim.keymap.set("v", "<localleader><leader>", "<ESC>")
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 
 -- move lines up and down
-vim.keymap.set("n", "<A-k>", "ddkP")
-vim.keymap.set("n", "<A-j>", "ddp")
+vim.keymap.set("n", "<A-k>", "ddkP", { silent = true })
+vim.keymap.set("n", "<A-j>", "ddp", { silent = true })
 
 -- move between panes with hjkl
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { silent = true })
 
 -- move between panes with arrows
-vim.keymap.set("n", "<C-Left>", ":wincmd h<CR>")
-vim.keymap.set("n", "<C-Down>", ":wincmd j<CR>")
-vim.keymap.set("n", "<C-Up>", ":wincmd k<CR>")
-vim.keymap.set("n", "<C-Right>", ":wincmd l<CR>")
+vim.keymap.set("n", "<C-Left>", ":wincmd h<CR>", { silent = true })
+vim.keymap.set("n", "<C-Down>", ":wincmd j<CR>", { silent = true })
+vim.keymap.set("n", "<C-Up>", ":wincmd k<CR>", { silent = true })
+vim.keymap.set("n", "<C-Right>", ":wincmd l<CR>", { silent = true })
 
--- select all 
-vim.keymap.set("n", "<C-a>", "ggVG")
+-- select all
+vim.keymap.set("n", "<C-a>", "ggVG", { silent = true })
 
+-- close buffer
+vim.keymap.set("n", "<leader>q", ":q<CR>")
+
+-- swap between last 2 buffers
+vim.keymap.set("n", "<leader>p", "<C-^>", {})
+
+-- jump to start/end of the line
+vim.keymap.set("n", "L", "$", {})
+vim.keymap.set("n", "H", "^", {})
+vim.keymap.set("v", "L", "$<left>", {})
+vim.keymap.set("v", "H", "^", {})
+
+-- move lines
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv")
+
+-- turn of serch results
+vim.keymap.set("n", "<leader>no", ":noh<CR>", { silent = true })
