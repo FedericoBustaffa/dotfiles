59162955dc39cabf7ecff1ec0493b299e7062958
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-05-07 20:05:40 +0200

    haskell nvim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,26 +2,27 @@
 -- serve per avere highlighting, e indentazione
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			modules = {},
-			auto_install = false,
-			ignore_install = {},
-			ensure_installed = {
-				"java",
-				"cpp",
-				"python",
-				"c",
-				"lua",
-				"latex",
-				"bibtex",
-			},
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            modules = {},
+            auto_install = false,
+            ignore_install = {},
+            ensure_installed = {
+                "java",
+                "cpp",
+                "python",
+                "c",
+                "lua",
+                "latex",
+                "bibtex",
+                "haskell",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
 }
