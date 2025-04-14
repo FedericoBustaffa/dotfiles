0dd94f14251278e897d5ae91f19b1dcd46dbd008
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-04-14 19:28:25 +0200

    nvim

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -45,5 +45,34 @@ return {
     'vague2k/vague.nvim',
     lazy = false,
     priority = 1000,
+    opts = {
+      style = {
+        -- "none" is the same thing as default. But "italic" and "bold" are also valid options
+        boolean = 'none',
+        number = 'none',
+        float = 'none',
+        error = 'none',
+        comments = 'italic',
+        conditionals = 'none',
+        functions = 'none',
+        headings = 'bold',
+        operators = 'none',
+        strings = 'italic',
+        variables = 'none',
+
+        -- keywords
+        keywords = 'none',
+        keyword_return = 'italic',
+        keywords_loop = 'none',
+        keywords_label = 'none',
+        keywords_exception = 'none',
+
+        -- builtin
+        builtin_constants = 'none',
+        builtin_functions = 'none',
+        builtin_types = 'none',
+        builtin_variables = 'none',
+      },
+    },
   },
 }
