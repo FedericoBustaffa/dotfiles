11180f14c95fa22a5c3f0ec9721204a14f425742
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-04-15 19:18:42 +0200

    snacks

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,9 +3,31 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
+    animate = { enabled = true },
     bigfile = { enabled = true },
-    notify = { enabled = true },
+    notifier = {
+      enabled = true,
+      timeout = 2000,
+    },
     bufdelete = { enabled = true },
     dashboard = { enabled = true },
+    git = { enabled = true },
+    lazygit = { enabled = true },
+  },
+  keys = {
+    {
+      '<leader>bd',
+      function()
+        Snacks.bufdelete()
+      end,
+      desc = 'Delete Buffer',
+    },
+    {
+      '<leader>lg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'LazyGit',
+    },
   },
 }
