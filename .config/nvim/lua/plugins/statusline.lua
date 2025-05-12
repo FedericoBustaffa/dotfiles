3b2a353eabd3257c8318b33bb61a2e41a7f95005
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-05-12 17:31:19 +0200

    wayland

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -13,6 +13,11 @@ return {
               cond = require('lazy.status').has_updates,
               color = { fg = '#ff9e64' },
             },
+            {
+              require('noice').api.statusline.mode.get,
+              cond = require('noice').api.statusline.mode.has,
+              color = { fg = '#ff9e64' },
+            },
           },
         },
       }
