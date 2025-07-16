b8cda4499ad4f82ff2bb64b1a309d2b9b3d1bb6c
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-07-16 11:28:23 +0200

    smear plugin

---
+++ nvim/.config/nvim/lua/plugins/smear.lua
@@ -0,0 +1,12 @@
+return {
+  'sphamba/smear-cursor.nvim',
+  opts = {
+    stiffness = 0.9,
+    trailing_stiffness = 0.9,
+    stiffness_insert_mode = 0.9,
+    trailing_stiffness_insert_mode = 0.8,
+    damping = 0.9,
+    damping_insert_mode = 0.8,
+    distance_stop_animating = 0.5,
+  },
+}
