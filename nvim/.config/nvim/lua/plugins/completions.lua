5b0176afbdfe1c42e082028c5e968c33755ba15d
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-07-27 12:33:59 +0200

    nvim signature completion

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -13,7 +13,7 @@ return {
     opts = {
       keymap = { preset = 'default' },
       appearance = { nerd_font_variant = 'mono' },
-      signature = { enabled = false },
+      signature = { enabled = true },
       completion = { documentation = { auto_show = false } },
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
