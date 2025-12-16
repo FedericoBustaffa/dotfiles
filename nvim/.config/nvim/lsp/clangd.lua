8f01386111d54e593447026716ece37323defbe7
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-12-16 01:43:36 +0100

    fix vague for nvim

--- nvim/.config/nvim/lsp/clangd.lua
+++ nvim/.config/nvim/lsp/clangd.lua
@@ -9,18 +9,26 @@ return {
     '--header-insertion=never',
     '--cross-file-rename',
   },
+
   filetypes = { 'c', 'h', 'cpp', 'hpp' },
+
   root_markers = {
     'Makefile',
     'CMakeLists.txt',
     '.git',
   },
+
+  on_attach = function(client, bufnr)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+
   settings = {
     clangd = {
       completion = { detailedLabel = true },
       diagnostics = true,
     },
   },
+
   capabilities = vim.tbl_deep_extend('force', {}, vim.lsp.protocol.make_client_capabilities(), blink.get_lsp_capabilities(), {
     fileOperations = {
       didRename = true,
