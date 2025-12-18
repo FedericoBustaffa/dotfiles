59557156e5beb3cf4608d9158b5529698cb26a4f
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-12-18 13:38:52 +0100

    remove ts aucomm

--- nvim/.config/nvim/lua/config/autocommands.lua
+++ nvim/.config/nvim/lua/config/autocommands.lua
@@ -48,9 +48,3 @@ vim.api.nvim_create_autocmd('BufReadPost', {
     end
   end,
 })
-
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function(args)
-    pcall(vim.treesitter.start, args.buf)
-  end,
-})
