2e767125ad0eccf750dab9857984a065fed10105
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-12-29 14:26:14 +0100

    colorscheme

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -27,68 +27,13 @@ return {
     },
   },
   {
-    'navarasu/onedark.nvim',
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require('onedark').setup {
-        style = 'dark',
-      }
-      require('onedark').load()
-    end,
-  },
-  {
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  {
     'dgox16/oldworld.nvim',
     lazy = false,
     priority = 1000,
   },
   {
-    'zenbones-theme/zenbones.nvim',
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = 'rktjmp/lush.nvim',
-    lazy = false,
-    priority = 1000,
-    -- you can set set configuration options here
-    -- config = function()
-    --     vim.g.zenbones_darken_comments = 45
-    --     vim.cmd.colorscheme('zenbones')
-    -- end
-  },
-  {
-    'Mofiqul/vscode.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  {
-    'armannikoyan/rusty',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  {
     'vague-theme/vague.nvim',
     lazy = false,
     priority = 1000,
-  },
-  {
-    'cdmill/neomodern.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('neomodern').setup {
-        theme = 'roseprime',
-        variant = 'dark',
-      }
-      require('neomodern').load()
-    end,
   },
 }
