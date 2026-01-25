6ba8c8863defd78eb6468bc41a16c87b2dc0f54f
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-01-25 13:54:31 +0100

    sync

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -29,7 +29,7 @@ fzf_uni() {
     FILE=$(find "$DIR" -type f -name "*.pdf" | fzf)
 
     if [[ -n "$FILE" ]]; then
-        nohup zathura "$FILE" &>/dev/null &
+        nohup sioyek "$FILE" &>/dev/null &
     fi
 }
 
