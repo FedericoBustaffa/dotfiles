ff8ef59b655cc3b79a78c997b56776dcea0bcaa3
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-02-09 16:27:19 +0100

    sync

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -15,7 +15,7 @@ return {
       keymap = { preset = 'default' },
       appearance = {
         use_nvim_cmp_as_default = false,
-        nerd_font_variant = 'normal',
+        nerd_font_variant = 'mono',
       },
       signature = { enabled = true },
       completion = {
