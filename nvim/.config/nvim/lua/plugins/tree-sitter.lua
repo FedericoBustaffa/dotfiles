0ba860ff7318cbc3ac28a7f84c057deb351e79a2
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-04-09 20:27:36 +0200

    sync

--- nvim/.config/nvim/lua/plugins/tree-sitter.lua
+++ nvim/.config/nvim/lua/plugins/tree-sitter.lua
@@ -1,25 +1,11 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  branch = 'master',
+  branch = 'main',
   lazy = false,
   build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = {
-        'bash',
-        'doxygen',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'lua',
-        'luadoc',
-        'vim',
-        'vimdoc',
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-      additional_vim_regex_highlighting = false,
-    }
-  end,
+  opts = {
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
 }
