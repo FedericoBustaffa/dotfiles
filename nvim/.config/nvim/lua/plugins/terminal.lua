305ee063d516229b65d2f6908c8358b07f98b2b8
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-04-14 18:33:20 +0200

    sync

--- nvim/.config/nvim/lua/plugins/terminal.lua
+++ nvim/.config/nvim/lua/plugins/terminal.lua
@@ -5,9 +5,6 @@ return {
     direction = 'float',
     float_opts = {
       border = 'curved',
-      width = function()
-        return vim.o.columns * 0.8
-      end,
     },
     open_mapping = [[<A-p>]],
   },
