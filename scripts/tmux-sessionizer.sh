d522392ac2fc8ba975e5231e9db7bf031f41cb3a
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-05-25 14:29:49 +0200

    sync

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/ ~/dotfiles ~/dev/py/ ~/dev/cpp/ ~/latex/ \
+    selected=$(find ~/ ~/dotfiles ~/dev/ ~/latex/ \
         -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
@@ -15,16 +15,16 @@ selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
+    tmux new-session -s "$selected_name" -c "$selected"
     exit 0
 fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
+if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
 if [[ -z $TMUX ]]; then
-    tmux attach -t $selected_name
+    tmux attach -t "$selected_name"
 else
-    tmux switch-client -t $selected_name
+    tmux switch-client -t "$selected_name"
 fi
