7c001dc8c104f24ad0973b693505f95eba95cb57
FedericoBustaffa <kiko.bustaffa@gmail.com>
2026-07-28 15:09:35 +0200

    sync

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ vim.lsp.config('*', {
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.rename, opts)
+    -- vim.keymap.set('n', 'gr', vim.lsp.buf.rename, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
